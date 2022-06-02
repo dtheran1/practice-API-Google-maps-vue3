55eb527f37c2352308fe91f56d281b3dbeaf2d8b
Dtheran <dtheran1@gmail.com>
2022-06-02 08:18:35 -0500

    init

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,43 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="d-flex text-center" style="height: 20vh">
+    <div class="m-auto">
+      <h4>Your Position</h4>
+      Latitude: {{ currPos.lat.toFixed(2) }}, Longitude:
+      {{ currPos.lng.toFixed(2) }}
+    </div>
+  </div>
+  <div ref="mapDiv" style="width: 100%; height: 80vh;"></div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+/* eslint-disable no-undef */
+import { computed, onMounted, ref } from "vue";
+import { useGeolocation } from "./useGeolocation.js";
 
+import { Loader } from "@googlemaps/js-api-loader";
+
+const GOOGLE_MAPS_API_KEY = "AIzaSyAbISdzV4THYbAgp6U1P0b5BpfMgxyM9lU";
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
-</script>
+  name: "App",
+  components: {},
+  setup() {
+    const { coords } = useGeolocation();
+    const currPos = computed(() => ({
+      lat: coords.value.latitude,
+      lng: coords.value.longitude,
+    }));
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+    const loader = new Loader({ apiKey: GOOGLE_MAPS_API_KEY });
+    const mapDiv = ref(null);
+    onMounted(async () => {
+      await loader.load();
+      new google.maps.Map(mapDiv.value, {
+        center: currPos.value, 
+        zoom: 15
+      });
+    });
+
+    return { currPos, mapDiv };
+  },
+};
+</script>
